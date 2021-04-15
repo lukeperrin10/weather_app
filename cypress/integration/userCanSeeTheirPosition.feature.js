c24fba523b3d1dccd5cd71f7e0d71a24533a4c43
DavDan1 <david-danielyan@hotmail.com>
2021-04-15 20:30:52 +0200

    implemented location and current weather,defined town and temp in class App

--- cypress/integration/userCanSeeTheirPosition.feature.js
+++ cypress/integration/userCanSeeTheirPosition.feature.js
@@ -1,26 +1,23 @@
-describe('weather info for user\`s location', () => {
-  it('is expected to be displayed on initial render', () => {
-    cy.visit('/', ({
+describe("weather info for user`s location", () => {
+  it("is expected to be displayed on initial render", () => {
+    cy.visit("/",( {
       onbeforeunload(window) {
         const stubLocation = {
           coords: {
-            latitude: 59.33245,
-            longitude: 18.06859,
-          }
+            latitude: 59.46081,
+            longitude: 17.88477,
+          },
         };
         cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
-          callback => {
-            return callback(stubLocation)
+          (callback) => {
+            return callback(stubLocation);
           }
-        )
-      }
-    }))
-    cy.get('[data-cy=weather-display]').within(() => {
-      cy.get('data-cy=temp').should('contain', '10°C');
-      cy.get('[data-cy=location]').should('contain', 'Stockholm');
-
-
+        );
+      },
+    }));
+    cy.get("[data-cy=weather-display]").within(() => {
+      cy.get("[data-cy=temp]").should("contain", "7°C");
+      cy.get("[data-cy=location]").should("contain", "Stockholm");
     });
   });
 });
-
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import axios from 'axios'
-import React, { Component } from 'react'
-
+import axios from "axios";
+import React, { Component } from "react";
 
 class App extends Component {
   state = {
-    geolocation: {}
-  }
-
-
-
+    geolocation: {},
+    town: {},
+    temp: {},
+  };
 
   componentDidMount() {
+    navigator.geolocation.getCurrentPosition(async (position)=> {
+      const openCageKey = process.env.REACT_APP_OPEN_CAGE_API_KEY;
+      const openWeatherMapKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 
-
-    navigator.geolocation.getCurrentPosition(async position => {
-      const openCageKey = process.env.REACT_APP_OPEN_CAGE_API_KEY
-      const openWeatherMapKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY
-
-      let { latitude, longitude } = position.coords
-      let locationResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?key=${openCageKey}&q=${latitude}%${longitude}&pretty=1&no_annotations=1`)
-      let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${openWeatherMapKey}&units=metric`)
-      debugger
-    })
+      let { latitude, longitude } = position.coords;
+      let locationResponse = await axios.get(
+        `https://api.opencagedata.com/geocode/v1/json?key=${openCageKey}&q=${latitude}+${longitude}&pretty=1&no_annotations=1`
+      );
+      let weatherResponse = await axios.get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${openWeatherMapKey}&units=metric`
+      );
+ 
+      let weatherInfo = {
+        town: locationResponse.data.results[0].components.town,
+        temp: weatherResponse.data.current.temp,
+      };
+      this.setState({ location: weatherInfo });
+      // debugger
+    });
   }
   render() {
-    return (
-      <div>
-        Hello world
-      </div>
-    )
+    return <div>Hello world</div>;
   }
 }
 
-export default App
+export default App;
